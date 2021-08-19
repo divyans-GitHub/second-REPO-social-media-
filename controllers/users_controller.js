293ca8fc9c1eb10ae8495fc583e1b81615902608
divyans-GitHub <divyanshuji123456@gmail.com>
2021-08-19 20:34:30 +0530

    complete layout code and use variable and fill as perrequirement

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,7 @@
 module.exports.profile = function(req , res){
      //res.end('<h1> WELCOME TO USERS PROFILE </h1>');
      return res.render('user_profile',{
-          
+        title: "PROFILE"
      });
 }
 // Here profile is called as action
