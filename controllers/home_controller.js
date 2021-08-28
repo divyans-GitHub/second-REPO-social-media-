9dafb0b03599257fa60a58e779b88b991d575431
divyans-GitHub <divyanshuji123456@gmail.com>
2021-08-28 22:54:40 +0530

    show user name on each post of user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,17 +5,29 @@ module.exports.home = function(req , res ){
    //console.log(req.cookies);
    //res.cookie('user_id' , 34);
 
-   Post.find({} , function(err , posts){
-            if(err){
-                console.log('error in findind post when home action is called');
-                return;
-            }
-            return res.render('home' , {
-                title: "codeial | HOME",
-                posts: posts // right side posts is one of args in above call back func.
-            });
-   });
+//    Post.find({} , function(err , posts){
+//             if(err){
+//                 console.log('error in findind post when home action is called');
+//                 return;
+//             }
+//             return res.render('home' , {
+//                 title: "codeial | HOME",
+//                 posts: posts // right side posts is one of args in above call back func.
+//             });
+//    });
 
+   // populate the user of each post
+  Post.find({}).populate('user').exec(function(err , posts){
+                                        if(err){
+                                            console.log('error in findind post when home action is called');
+                                            return;
+                                        }
+                                        return res.render('home' , {
+                                            title: "codeial | HOME",
+                                            posts: posts // right side posts is one of args in above call back func.
+                                        });
+                                       }
+                                    );
    
     // res.end('<h1>express is up for codeial </h1>');
 }
