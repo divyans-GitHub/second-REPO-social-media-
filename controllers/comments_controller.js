b0e4112a02cb9240a82895bbf5f6c36239e511e1
divyans-GitHub <divyanshuji123456@gmail.com>
2021-09-04 12:03:26 +0530

    add delete comment feature by user who commented

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -31,3 +31,31 @@ module.exports.create = function(req , res ){
 
 }
 
+// to destroy a comment
+module.exports.destroy = function(req , res ){
+ Comment.findById(req.params.id , function(err , comment){
+     if(err){
+         console.log("Error in finding comment which is requested for delete");
+         return;
+     }
+     if(comment){
+            if(comment.user == req.user.id ){
+                let postId = comment.post;
+
+                comment.remove();
+
+                Post.findByIdAndUpdate(postId , { $pull: {comments: req.params.id}} , function(err , post){
+                      if(err){console.log('error in finding comment inpost'); return;}
+                      return res.redirect('back');
+                } );
+                
+              
+            }
+     }else{
+        return res.redirect('back'); 
+     }
+ });
+
+}
+
+
