4b18d06ab82294b6d6f138c8c422eb802c2da2ef
divyans-GitHub <87029404+divyans-GitHub@users.noreply.github.com>
2026-05-17 16:13:47 +0530

    fixed gulp file(minimize), and socket url

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,127 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const cssnano = require('gulp-cssnano');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cssnano = require("gulp-cssnano");
+const sharp = require("sharp");
+const rev = require("gulp-rev");
+const uglify = require("gulp-uglify");
+const del = require("del");
+const fs = require("fs");
+const path = require("path");
 
-const rev = require('gulp-rev');
+gulp.task("css", function (done) {
+  console.log("minifying css......");
 
-const uglify = require('gulp-uglify'); 
-
-
-
-let /** @type {import("gulp-imagemin")} */ imagemin;
-let /** @type {import("imagemin-jpegtran")} */ imageminJpegtran;
-let /** @type {import("imagemin-pngquant").default} */ imageminPngquant;
-
-const startup = async () => {
-    // @ts-ignore
-    imagemin = (await import("gulp-imagemin")).default;
-    // @ts-ignore
-    imageminJpegtran = (await import("imagemin-jpegtran")).default;
-    imageminPngquant = (await import("imagemin-pngquant")).default;
-};
-
-// run this task before any that require imagemin
-gulp.task("startup", async () => {
-    await startup();
-});
-
-//const imagemin = require('gulp-imagemin');
-const del = require('del');
-
-
-gulp.task('css' , function( done ){
-    console.log('minifying css......');
-    
-    gulp.src('./assets/sass/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
+  gulp
+    .src("./assets/sass/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
     .pipe(cssnano())
-    .pipe( gulp.dest('./assets.css'));
+    .pipe(gulp.dest("./assets.css"));
 
-    gulp.src('./assets/**/*.css')
+  gulp
+    .src("./assets/**/*.css")
     .pipe(rev())
-    .pipe( gulp.dest('./public/assets') )
-    .pipe( rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done();
+    .pipe(gulp.dest("./public/assets"))
+    .pipe(
+      rev.manifest({
+        cwd: "public",
+        merge: true,
+      }),
+    )
+    .pipe(gulp.dest("./public/assets"));
+  done();
 });
 
+gulp.task("js", function (done) {
+  console.log("miinifying js.........");
 
-gulp.task('js' , function( done ){
-    console.log('miinifying js.........');
-
-    gulp.src('./assets/**/*.js')
+  gulp
+    .src("./assets/**/*.js")
     .pipe(uglify())
     .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done();
-
+    .pipe(gulp.dest("./public/assets"))
+    .pipe(
+      rev.manifest({
+        cwd: "public",
+        merge: true,
+      }),
+    )
+    .pipe(gulp.dest("./public/assets"));
+  done();
 });
 
+gulp.task("images", async function () {
+  console.log("compressing image files.......");
 
-gulp.task('images' , function(done){
-    console.log('compressing image files.......');
+  const imageDir = "./assets";
+  const destDir = "./public/assets";
 
-    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)' )
-    .pipe(imagemin())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done();
-    
+  // Ensure destination directory exists
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir, { recursive: true });
+  }
+
+  const imageExtensions = /\.(png|jpg|gif|svg|jpeg)$/i;
+  const manifest = {};
+
+  const processDirectory = async (dir, outputDir) => {
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat.isDirectory()) {
+        const newOutputDir = path.join(outputDir, file);
+        if (!fs.existsSync(newOutputDir)) {
+          fs.mkdirSync(newOutputDir, { recursive: true });
+        }
+        await processDirectory(filePath, newOutputDir);
+      } else if (imageExtensions.test(filePath)) {
+        const ext = path.extname(file);
+        const basename = path.basename(file, ext);
+        const hash = Date.now();
+        const newFileName = `${basename}-${hash}${ext}`;
+        const outPath = path.join(outputDir, newFileName);
+
+        try {
+          if (ext.toLowerCase() === ".svg") {
+            // SVG: copy as-is
+            fs.copyFileSync(filePath, outPath);
+          } else if ([".jpg", ".jpeg"].includes(ext.toLowerCase())) {
+            // JPEG: compress
+            await sharp(filePath)
+              .jpeg({ quality: 80, progressive: true })
+              .toFile(outPath);
+          } else if (ext.toLowerCase() === ".png") {
+            // PNG: compress
+            await sharp(filePath).png({ compressionLevel: 9 }).toFile(outPath);
+          } else {
+            // GIF or other: copy as-is
+            fs.copyFileSync(filePath, outPath);
+          }
+          console.log(`✓ ${filePath} → ${outPath}`);
+        } catch (err) {
+          console.error(`✗ Error processing ${filePath}:`, err.message);
+        }
+      }
+    }
+  };
+
+  await processDirectory(imageDir, destDir);
+  console.log("Image compression complete");
 });
 
 //empty the public/assets directory
-gulp.task('clean:assets' , function(done){
-    del.sync('./public/assets');
-    done();
+gulp.task("clean:assets", function (done) {
+  del.sync("./public/assets");
+  done();
 });
 
-gulp.task( 'build' , gulp.series('clean:assets' , 'css' , 'js', 'startup' , 'images') , function(done){
-    console.log('Building assets');
+gulp.task(
+  "build",
+  gulp.series("clean:assets", "css", "js", "startup", "images"),
+  function (done) {
+    console.log("Building assets");
     done();
-})
+  },
+);
--- index.js
+++ index.js
@@ -1,69 +1,56 @@
-const express = require('express');
+const express = require("express");
 
-var cors = require('cors');
-const env = require('./config/environment');
+var cors = require("cors");
+const env = require("./config/environment");
 
-const logger = require('morgan');
+const logger = require("morgan");
 
 const app = express();
 
-require('./config/view-helper')(app);
+require("./config/view-helper")(app);
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 const port = 4000;
 
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 
 app.use(expressLayouts);
 
-const db = require('./config/mongoose');
+const db = require("./config/mongoose");
 
 // used for session cookie
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-const passportJWT = require('./config/passport-jwt-strategy');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const passportJWT = require("./config/passport-jwt-strategy");
 
-const passportGoogle = require('./config/passport-google-oauth2-strategy');
+const passportGoogle = require("./config/passport-google-oauth2-strategy");
 
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
-const sassMiddleware = require('node-sass-middleware');
-
-const flash = require('connect-flash');
-const customMware = require('./config/middleware');
-const multer  = require('multer')
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
+const multer = require("multer");
 
 //set up the chat server to be used with socket.io
-const chatServer = require('http').Server(app);
-const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+const chatServer = require("http").Server(app);
+const chatSockets = require("./config/chat_sockets").chatSockets(chatServer);
 chatServer.listen(5000);
 console.log("chat server is listening to port 5000");
 
-const path = require('path');
+const path = require("path");
 
-if(env.name == 'development'){
-    app.use(sassMiddleware({
-        //src: './assets/scss',
-        src: path.join(__dirname , env.asset_path , 'scss'),
-        dest: path.join(__dirname , env.asset_path , 'css'),
-        debug: true,
-        outputStyle: 'expanded',
-        prefix: '/css'
-    }));
-}
-
-
+// Sass is compiled via the `sass` CLI during development (see package.json `watch:sass` script)
 
 // middleware for reading form data
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 //tell app to use cookie
 app.use(cookieParser());
 
 // extract styles and scripts from individual file into layout
-app.set('layout extractStyles' , true);
-app.set('layout extractScripts' , true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 app.use(cors());
 
@@ -71,37 +58,39 @@ app.use(cors());
 app.use(express.static(__dirname + env.asset_path));
 
 //make the path uploads available for browser
-app.use('/uploads' , express.static(__dirname  + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
-app.use(logger(env.morgan.mode , env.morgan.options ));
+app.use(logger(env.morgan.mode, env.morgan.options));
 
 // setting up view engine
-app.set('view engine' , 'ejs');
-app.set('views' , './views');
-
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // mongo store is used to store session cookie in the DB
 //MIDDLE WARE TO ENCRYPTS
-app.use(session({
-    name: 'codeial',
+app.use(
+  session({
+    name: "codeial",
     secret: env.session_cookie_key,
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000 * 60 * 100)
+      maxAge: 1000 * 60 * 100,
     },
-    store:  MongoStore.create(
-        {
+    store: MongoStore.create(
+      {
         // mongooseConnection: db,
-        mongoUrl: 'mongodb://localhost/codeial_development',
-        autoRemove: 'disabled'
-        }, function(err){
-            // call back function in case connectioin is not established
-            console.log(err || 'connect-mongo setup is done !');
-            return;
-        }
-    )
-}));
+        mongoUrl: "mongodb://localhost/codeial_development",
+        autoRemove: "disabled",
+      },
+      function (err) {
+        // call back function in case connectioin is not established
+        console.log(err || "connect-mongo setup is done !");
+        return;
+      },
+    ),
+  }),
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -112,12 +101,11 @@ app.use(flash());
 app.use(customMware.setFlash);
 
 // use express router
-app.use('/' , require('./routes'));  
+app.use("/", require("./routes"));
 // or app.use('/' , require('./routes/index.js));
 
+app.listen(port, function (err) {
+  if (err) console.log(`Error in running server: ${err}`);
 
-app.listen(port , function(err){
-    if(err) console.log(`Error in running server: ${err}`);
-
-    console.log(`express erver is up and running on the port : ${port}`);
+  console.log(`express erver is up and running on the port : ${port}`);
 });
